be56a2e02aee288dd16e89bbc6b0fee9380ac97e
CommandLine <98483@acs.org>
2020-09-03 20:22:08 -0400

    removed old code added graph code

--- static/js/logic_v7.js
+++ static/js/logic_v7.js
@@ -18,13 +18,7 @@
 //    5) click on SSA scatter graph buttons:
 
 var endpoint = '/ultimate'
-  var selected_country = ''
-
-var indexSelector_line1 = 1; 
-var indexSelector_line2 = 1;
-var indexSelector = 0;
-var fdi_vs_metric = 0;
-var chosenLayer
+var selected_country = 'All African Countries'
 
 // Creating map object
 
@@ -42,8 +36,6 @@ var baseMaps
   var corruption
 
   var scatter_array = [] // array will contain the above objects. will be used to build the scatter graph
-  var selected_country_json_array = [] //?????
-
 
   var clickflag = 0 /// testing click envent gold
   var old_layer = '' // testing click envent gold
@@ -54,8 +46,142 @@ var baseMaps
   $('.index').hide()
   $('.scatter').hide()
 
-  var country_dict = []
-  var dict_key = 0
+  
+var gdp_radio_button_flag = 0
+var diversity_radio_button_flag = 0
+var corr_radio_button_flag = 0
+
+var selected_country_endpointData
+var country_data_to_be_graphed = []
+
+var xAxis_values = []
+var yAxis_conflict_values = []
+var yAxis_gdp_values = []
+var yAxis_diversity_values = []
+var yaxis_corr_values = []
+
+// vvvvvvvvvvvvvvvvvvvvvvvvvv Build Graphs vvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+function buildGraph(){
+  var first_trace = {
+    x: xAxis_values,
+    y: yAxis_gdp_values,
+    name: `gdp`,
+    type: 'line',
+    line: {color: 'green'
+    },
+    connectgaps: true
+  }
+
+  var second_trace = {
+    x: xAxis_values,
+    y: yAxis_conflict_values,
+    name: `conflict`,
+    yaxis: 'y2',
+    type: 'bar',
+    line: {color: '#ffffff'
+    },
+    connectgaps: true
+  }
+
+  var third_trace = {
+    x: xAxis_values,
+    y: yAxis_diversity_values,
+    name: `diversity`,
+    yaxis: 'y2',
+    type: 'line',
+    line: {color: 'yellow'
+    },
+    connectgaps: true
+  }
+
+  var fourth_trace = {
+    x: xAxis_values,
+    y: yaxis_corr_values,
+    name: `corruption`,
+    yaxis: 'y2',
+    type: 'line',
+    line: {color: 'blue'
+    },
+    connectgaps: true
+  }
+
+  if(gdp_radio_button_flag == 1){
+    data = [first_trace]
+    gdp_radio_button_flag = 0
+  }
+
+  else if(diversity_radio_button_flag == 1){
+    data = [third_trace]
+    diversity_radio_button_flag = 0
+  }  
+
+  else if(corr_radio_button_flag == 1){
+    data = [fourth_trace]
+    corr_radio_button_flag = 0
+  }
+
+  else{
+    data = [second_trace,first_trace,third_trace,fourth_trace]
+  }
+
+  var layout = {
+    title: 'first graph',
+    yaxis: {title: 'first graph'},
+    color:'black',
+    yaxis2: {
+      title: 'first graph',
+      overlaying: 'y',
+      side: 'right'
+    },
+    showlegend: true,
+      legend: {"orientation": "h"},
+    connectgaps: true
+  };
+  var config2 = {responsive : true}
+  Plotly.newPlot("one", data, layout, config2)
+}
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Build Graphs ^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+// vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv Retrive data from Endpoint and filter using selected country vvvvvvvvvvvvvv     
+function getSelectedCountryDataFromEndpoint(){
+  if(selected_country != 'All African Countries'){
+    d3.json(endpoint).then(endpointData => {
+      selected_country = d3.select('#selDataset').node().value
+      selected_country_endpointData = endpointData.filter(epd => epd.country_name == selected_country )
+      //
+      xAxis_values = []
+      yAxis_conflict_values = []
+      yAxis_gdp_values = []
+      yAxis_diversity_values = []
+      yaxis_corr_values = []
+  
+      selected_country_endpointData.forEach(sced => {
+        xAxis_values.push(sced.year)
+        yAxis_conflict_values.push(sced.conflict_events)
+        yAxis_gdp_values.push(sced.gdp_total)
+        yAxis_diversity_values.push(sced.ethnic_score)
+        yaxis_corr_values.push(sced.corruption_control_estimate)
+      })
+      buildGraph()
+    })
+  }
+
+  else{
+    d3.json(endpoint).then(endpointData => {
+      selected_country = d3.select('#selDataset').node().value
+      console.log(endpointData)
+
+    })
+
+  }
+}
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Retrive data from Endpoint and filter using selected country ^^^^^^^^^^^^^^^^^
+getSelectedCountryDataFromEndpoint()
+
+d3.select('#selDataset').on('change',getSelectedCountryDataFromEndpoint)
+
+
 
  
   // Grab data with d3
@@ -431,18 +557,18 @@ function mouseout_func(event) {
       };
 
         baseMaps = {
-          "Diversity Rate": ethnicity,
-          "GDP Growth Rate": gdpGrowth,
           "Instances of Conflict": conflict,
+          "Diversity Rate": ethnicity,
+          "GDP Growth Rate": gdpGrowth,    
           "Corruption Score": corruption
         }
       
         var myMap = L.map("map", {
           center: [-1.170320, 23.241192],
           zoom: 3,
-          layers: ethnicity
+          layers: conflict
         });
-            legend.addTo(myMap);
+            legendMort.addTo(myMap);
 
           L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
             attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -465,287 +591,41 @@ function mouseout_func(event) {
             myMap.removeLayer(outline)
             outline.addTo(myMap)
             }
+           
+            
             
 
           if(eventlayer.name == 'GDP Growth Rate'){
+            gdp_radio_button_flag = 1
             legendCase.addTo(myMap)
+            if(selected_country != ''){
+              getSelectedCountryDataFromEndpoint()
+            }
           }
+
           if(eventlayer.name == 'Diversity Rate'){
+            diversity_radio_button_flag = 1
             legend.addTo(myMap)
+            if(selected_country != ''){
+              getSelectedCountryDataFromEndpoint()
+            }
           }
+
           if(eventlayer.name == 'Instances of Conflict'){
             legendMort.addTo(myMap)
+            if(selected_country != ''){
+              getSelectedCountryDataFromEndpoint()
+            }
           }
+
           if(eventlayer.name == 'Corruption Score'){
+            corr_radio_button_flag = 1
             legendCorupt.addTo(myMap)
+            if(selected_country != ''){
+              getSelectedCountryDataFromEndpoint()
+            }
           }
         })
-
-
-        
-  //(BSGSSA)vvvvvvvvvvvvvvvv BUILD SCATTER GRAPH vvvvvvvvvvvvvvvvvvv
-    function build_scatter(scatter_array){
-      var text = Object.keys(scatter_array[0])
-      var x_axis = Object.values(scatter_array[0])
-      var y_axis = Object.values(scatter_array[fdi_vs_metric])           
-
-      var trace1 = {
-        x: x_axis,
-        y:  y_axis,
-        mode: 'markers',
-        type: 'scatter',
-        name: 'Sub-Saharan Country',
-        text: text,
-        marker: { size: 12 }
-      };
-      
-      var data = [trace1];
-      
-      var layout = {
-        xaxis: {
-          title: `${selected_country_json_array[0][indexSelector]['series']}`
-        },
-        yaxis: {
-          title: `${selected_country_json_array[fdi_vs_metric][0]['series']}`
-        },
-        title:`${selected_country} ${selected_country_json_array[0][indexSelector]['series']} vs ${selected_country_json_array[fdi_vs_metric][0]['series']} `,
-        showlegend: true,
-          legend: {"orientation": "h"}
-      };
-
-      
-      Plotly.newPlot('one', data, layout);
-
-  }    
-  // (BSGSSA)^^^^^^^^^^^^^^^^^^^^^ BUILD SCATTER GRAPH ^^^^^^^^^^^^^^^ 
-
- 
-//(SD) vvvvvvvvvvvvvvvv SCATTER PLOT: BUILDING AN OBJECT from every endpoint for the year 2018 vvvvvvvvvvvvvvvvvvvv
-//vvvvvvvvvvvvvvvv ie. for the FDI endpoint the result will be {country_name:amount of FDI injected} vvvvvvvvv
-//vvvvvvvvvvvvvvvv assumption: it will take 5 years for the effects of FDI to take place vvvvvvvvvvvvvvvvvvvvv
-  function scatter_data(){
-
-    indexEndPoints.forEach(iep => {
-      d3.json(iep).then(data => {
-        if (indexEndPoints.length > 0){
-          data.forEach(c => {
-
-            var obj_key = c.country_name
-            
-            if (c.series == 'FDI'){
-              var obj_val = c['2005']
-            }
-            else {
-              var obj_val = c['2009']
-            }
-        
-            if (c.series == 'FDI') { 
-              fdi_obj[`${obj_key}`] = obj_val
-              scatter_array[0] = (fdi_obj)
-            }
-
-            else if (c.series == 'GDP') { 
-              gdp_obj[`${obj_key}`] = obj_val
-              scatter_array[1] = (gdp_obj)
-            }
-
-            else if (c.series == 'GNI Per Capita') { 
-              gni_c_obj[`${obj_key}`] = obj_val
-              scatter_array[2] = (gni_c_obj) 
-            }   
-
-            else if (c.series == 'GDP Per Capita') { 
-              gdp_c_obj[`${obj_key}`] = obj_val 
-              scatter_array[3] = (gdp_c_obj) 
-            }
-
-            else if (c.series == 'Health Expenditure') { 
-              he_obj[`${obj_key}`] = obj_val
-              scatter_array[4] = (he_obj) 
-            }
-
-            else if (c.series == 'Life Expectancy') { 
-              le_obj[`${obj_key}`] = obj_val
-              scatter_array[5] = (le_obj)            
-            }
-
-            else if (c.series == 'Human Development Index') {
-              hd_obj[`${obj_key}`] = obj_val
-              scatter_array[6] = (hd_obj)           
-            }
-
-            else if (c.series == 'Education Index') { 
-              edu_obj[`${obj_key}`] = obj_val
-              scatter_array[7] = (edu_obj)
-            }
-
-            else if (c.series == 'Political Stability') { 
-              ps_obj[`${obj_key}`] = obj_val
-              scatter_array[8] = (ps_obj)
-            }
-
-            else if (c.series == 'Corruption Index') { 
-              corr_obj[`${obj_key}`] = obj_val
-              scatter_array[9] = (corr_obj)
-            }
-
-          })
-        }
-      build_scatter(scatter_array)
-      }); 
-    }); 
-  };
-// (SD)^^^^^^^^^^^^^^^^ SCATTER PLOT: BUILDING AN OBJECT from every endpoint for the year 2018 ^^^^^^^^^^^^^^^^^^^^
-//^^^^^^^^^^^^^^^^ ie. for the FDI endpoint the result will be {country_name:amount of FDI injected} ^^^^^^^^^
-//^^^^^^^^^^^^^^^^ assumption: it will take 5 years for the effects of FDI to take place ^^^^^^^^^^^^^^^^^^^^^
-
-    
-//(BG) vvvvvvvvvvvvvvvvvvv BUILDING COUNTRY GRAPH vvvvvvvvvvvvvvvvvvvvvv
-  function build_graph() {
-     // console.log(selected_country_json_array)
-    var x_fdi = Object.keys(selected_country_json_array[indexSelector][0]).slice(0,19);
-    var y_fdi = Object.values(selected_country_json_array[indexSelector][0]).slice(0,19);
-    //console.log(x_fdi)
-  //   console.log(y_fdi)
-
-
-    var x_gdp_c = Object.keys(selected_country_json_array[indexSelector_line1][0]).slice(0,19);
-    var y_gdp_c = Object.values(selected_country_json_array[indexSelector_line1][0]).slice(0,19)
-  //   console.log(x_gdp_c)
-  //   console.log(y_gdp_c)
-
-    var x_gni_c = Object.keys(selected_country_json_array[indexSelector_line2][0]).slice(0,19);
-    var y_gni_c = Object.values(selected_country_json_array[indexSelector_line2][0]).slice(0,19)
-  //   console.log(x_gni_c)
-  //   console.log(y_gdp_c)
-
-      var fdi_trace = {
-        x: x_fdi,
-        y: y_fdi,
-        name: `FDI`,
-        type: 'bar',
-        marker: {color: '#DAA520',
-        },
-        connectgaps: true
-      }
-      
-      var first_trace = {
-        x: x_gdp_c,
-        y: y_gdp_c,
-        name: `${selected_country_json_array[indexSelector_line1][0]['series']}`,
-        yaxis: 'y2',
-        type: 'line',
-        line: {color: 'red'
-        },
-        connectgaps: true
-      }
-
-      var second_trace = {
-          x: x_gni_c,
-          y: y_gni_c,
-          name: `${selected_country_json_array[indexSelector_line2][0]['series']}`,
-          yaxis: 'y2',
-          type: 'line',
-          line: {color: 'blue'
-          },
-          connectgaps: true
-        }
-
-      var data = [fdi_trace, first_trace, second_trace]
-
-      var layout = {
-        title: `${selected_country_json_array[0][0]['country_name']} FDI vs. ${selected_country_json_array[indexSelector_line1][0]['series']} & ${selected_country_json_array[indexSelector_line2][0]['series']}`,
-        yaxis: {title: 'FDI'},
-        yaxis2: {
-          title: `${selected_country_json_array[indexSelector_line1][0]['series']} & ${selected_country_json_array[indexSelector_line2][0]['series']}`,
-          overlaying: 'y',
-          side: 'right'
-        },
-        showlegend: true,
-          legend: {"orientation": "h"},
-        connectgaps: true
-      };
-      var config2 = {responsive : true}
-      Plotly.newPlot("one", data, layout, config2)
-  }
-//(BG)^^^^^^^^^^^^^^^^ BUILDING COUNTRY GRAPH ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-
-
-//(FEPJ)vvvvvvvvvvvvvvvvv USE SELECTED COUNTRY FROM DROPDOWN/MAP TO FILTER GEOJSON vvvvvvvvvvvvvvvvvvvvvvv 
-//Not used in new geojson
-//data filter
-  // function filter_endpointJson(selected_country){ 
-  //   indexEndPoints.forEach(iep => {
-      
-  //     d3.json(iep).then(data => {
-  //       if (indexEndPoints.length > 0){
-  //         var selected_country_json = data.filter(d => selected_country == d.country_name)
-  //         selected_country_json_array[selected_country_json[0]['id']] = selected_country_json
-  //       }
-  //       //PLOT Construction
-  //       build_graph()
-
-  //       buildsnapshot()
-  //     }); 
-  //   });    
-  // };
-//(FEPJ)^^^^^^^^^^^^^^^^^^^^^ USE SELECTED COUNTRY FROM DROPDOWN/MAP TO FILTER GEOJSON ^^^^^^^^^^^^^^^^^^^
-
-
-
-//(FEPJDD)vvvvvvvvvvvvvv SET SELECTED COUNTRY TO VALUE OF DROP DOWN SELECTION vvvvvvvvvvvvvvv
-function filter_endpointJsonDD(){
-  indexSelector = 0;
-  selected_country = d3.select('#selDataset').node().value
-
-  if (selected_country != 'All Sub-Saharan Countries') {
-    $('.index').show()
-    $('.scatter').hide()
-    indexSelector_line1 = 2 // put to sleep: display no line graphs
-    indexSelector_line2 = 3 // put to sleep: display no line graphs
-    filter_endpointJson(selected_country)  
-    //make a call to the click listener function 
-  }
-
-  else {
-    $('.index').hide()
-    $('.scatter').show()
-
-    scatter_data();
-    fdi_vs_metric = 1
-  }
-
-  /// to identify each contires index in the geojeson
-      data.features.forEach(c => {
-          country_dict.push({
-              key: dict_key,
-              value: c.properties.name 
-          })
-          dict_key++
-      }) 
-      console.log(country_dict)
-
-      var country_obj = country_dict.filter(c => selected_country == c.value )
-      polygon_country_indx = country_obj[0]['key']
-      var x = document.getElementsByClassName("leaflet-interactive")[polygon_country_indx];
-      var att = document.createAttribute("id"); 
-      att.value = `${selected_country}`; 
-      x.setAttributeNode(att)
-      console.log(x)
-
-      $('#Angola').find('path').trigger('click'); // Works
-
-     
-  /// to identify each contires index in the geojeson
-
-};
-  //(FEPJDD)^^^^^^^^^^^^^^^ SET SELECTED COUNTRY TO VALUE OF DROP DOWN SELECTION  ^^^^^^^^^^^^^^
-
-
-// (DDEL) Drop down event listener: a change in the drop down value will call function filter_endpointJsonDD
-d3.select("#selDataset").on("change",filter_endpointJsonDD)
-//d3.select("#selDataset").on("change",add_ticker_function_call_here) //// this is for Chris. 
 
 
 ////////// test: checkbox event listener ///////////////////////////////////// CheckBox
@@ -760,66 +640,8 @@ function handleChange(){
         console.log('GDP/Capita: no')
     }
 }
-
+///////// test: checkbox event listener ///////////////////////////////////// CheckBox
 d3.select("#corr-chkbx").on('change',handleChange)
-////////// test: checkbox event listener ///////////////////////////////////// CheckBox
-
-
-
-
-//// TEST///////// connect drop down selection to map: access polygons from using dropdown listiner ///////
-
-
-//// TEST///////// connect drop down selection to map: access polygons from using dropdown listiner ///////
-
-
-
-
-  //vvvvvvvvvvvvvvvvvvv BUTTON EVENT LISTENERS & GRAPH CALIBRATION FUNCTIONS vvvvvvvvvvvvvvvvv
-  // Called after a button is clicked:
-  // sets the indexSelector to the value that corresponds to the data file id
-  // calls the filter_endpointjson 
-  function life_click(){
-    indexSelector_line1 = 4
-    indexSelector_line2 = 5
-    filter_endpointJson(selected_country)
-  };
-
-  function econIndx_click(){
-    indexSelector_line1 = 2
-    indexSelector_line2 = 3
-    filter_endpointJson(selected_country)
-  };
-
-  function poliIndx_click(){
-      indexSelector_line1 = 8
-      indexSelector_line2 = 9
-      filter_endpointJson(selected_country)
-  };
-
-  function fdiVSgdp_click(){
-    scatter_data();
-    fdi_vs_metric = 1
-  };
-
-  function fdiVShumdev_click(){
-    scatter_data();
-    fdi_vs_metric = 6
-  };
-
-  function fdiVScorr_click(){
-    scatter_data();
-    fdi_vs_metric = 9
-  };
-
-  //button listenetrs 
-  d3.select('#lifeIndx-btn').on('click', life_click);
-  d3.select('#poliIndx-btn').on('click', poliIndx_click);
-  d3.select('#econIndx-btn').on('click', econIndx_click);
-  d3.select('#fdi_gdp-btn').on('click', fdiVSgdp_click);
-  d3.select('#fdi_humnDev-btn').on('click', fdiVShumdev_click);
-  d3.select('#fdi_corr-btn').on('click',fdiVScorr_click);
-  //^^^^^^^^^^^^^^^^^^^^^^^^^ BUTTON EVENT LISTENERS & GRAPH CALIBRATION FUNCTIONS ^^^^^^^^^^^^^
 
 })
 
@@ -876,7 +698,8 @@ Highcharts.chart('container', {
           name: "Mortality Rate",
           value: 0.088169
       
-      }]
+      }],
+      color: '#CA3433'
   }, {
       name: 'Economic Factors',
       data: [{
@@ -895,6 +718,7 @@ Highcharts.chart('container', {
           name: "FDI Inflows GDP",
           value: 0.042612
       }],
+      color: '#95F985'
   }, {
       name: 'Government Factors',
       data: [{
@@ -908,7 +732,9 @@ Highcharts.chart('container', {
       {
           name: "Government Stability",
           value: 0.079769
-      }]
+      }],
+      color: 'goldenrod'
+
   } 
 ]
 });
